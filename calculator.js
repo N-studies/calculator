4cf5337ca0d731a950060e4def22d01057631992
N-studies <122186098+N-studies@users.noreply.github.com>
2023-03-01 14:02:56 -0500

    fix buttons

--- calculator.js
+++ calculator.js
@@ -1,29 +1,53 @@
-function add(a,b) {
-  return a + b
+function add(a, b) {
+  return a + b;
 }
 
-function subtract(a,b) {
-  return (a - b)
+function subtract(a, b) {
+  return a - b;
 }
 
-function multiply(a,b) {
-  return a * b
+function multiply(a, b) {
+  return a * b;
 }
 
-function divide(a,b) {
-  return a/b
+function divide(a, b) {
+  return a / b;
 }
 
-function operate(a,b,operation) {
-    switch (operation){
-        case "+" : return add(a,b);
-        break;
-        case "-" : return subtract(a,b);
-        break;
-        case "*" : return multiply(a,b);
-        break;
-        case "/" : return divide(a,b);
-        break;
-    }
+function operate(a, b, operation) {
+  switch (operation) {
+    case "+":
+      return add(a, b);
+      break;
+    case "-":
+      return subtract(a, b);
+      break;
+    case "*":
+      return multiply(a, b);
+      break;
+    case "/":
+      return divide(a, b);
+      break;
+  }
+}
+function clear(arr) {
 
 }
+
+let value1;
+let value2;
+let storage = []
+console.log(storage.length)
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    
+    storage.push(button.textContent)
+    console.log(storage)
+       
+  });
+
+  
+});
+
+ 
