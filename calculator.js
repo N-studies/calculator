395d9d549a8d93c888ab7d8b62368e7b133091c1
N-studies <122186098+N-studies@users.noreply.github.com>
2023-03-02 09:45:58 -0500

    Attempt to add button functionality

--- calculator.js
+++ calculator.js
@@ -31,23 +31,47 @@ function operate(a, b, operation) {
   }
 }
 function clear(arr) {
-
+  if (arr.length != 0);
+  {
+    arr.splice(0, arr.length);
+  }
 }
 
-let value1;
-let value2;
-let storage = []
-console.log(storage.length)
+
+let value1 = 0;
+let value2 = 0;
+let storage = [];
+let operation;
 const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
+    if (button.className == "number") {
+      storage.push(Number(button.textContent));
+      value1 = storage.join("");
+      
+    } else if (button.classList == "clear") {
+      clear(storage);
+
+      //else if operator,delete storage and use it for value2 
+    } else if (button.className == "operations") {
+      clear(storage);
+      operation = button.textContent;
+
+
+    }
+     // else if, when equal is pressed operate
+    else if (button.classList == "equal") {
+      operate(value1,value2,operation);
+      
+      
+    }
+   
+    //console.log(button);
+    console.log(storage);
     
-    storage.push(button.textContent)
-    console.log(storage)
-       
+    console.log(value1);
+    console.log(value2);
+    operate(value1,value2,operation);
+
   });
-
-  
 });
-
- 
