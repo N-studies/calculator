f77c6b64e2b744d93f9717c2f0f9a1a2ec6d9ad2
N-studies <122186098+N-studies@users.noreply.github.com>
2023-03-08 16:00:47 -0500

    attempt at refactor

--- calculator.js
+++ calculator.js
@@ -31,12 +31,79 @@ function operate(a, b, operation = "+") {
       break;
   }
 }
-function clear(arr) {
-  if (arr.length != 0);
-  {
-    arr.splice(0, arr.length);
-  }
+function clear(arr) { 
+  return arr.splice(0, arr.length);
 }
+
+function firstValue(arr,btn) {
+   arr.push(btn.textContent)
+   return Number(arr.join(""))
+}
+
+function secondValue(number) {
+  // How do we get the second value?
+}
+
+function storage(arr,btn) {
+  // Use this to push items from button event listener to an array
+  arr.push(btn.textContent)
+}
+let screen = document.querySelector(".screen")
+let numberStore1 = []
+let numberStore2 = []
+let operation
+let storedValue 
+let storedValue2
+let result
+const buttons = document.querySelectorAll("button")
+buttons.forEach((button) => { 
+button.addEventListener("click",() => {
+ if (button.className == "number" && operation == undefined) {
+  storedValue = firstValue(numberStore1,button)
+  screen.textContent = storedValue
+ 
+ }
+ if (button.className == "clear") {
+  clear(numberStore1)
+  clear(numberStore2)
+  operation = undefined
+  screen.textContent = 0
+  storedValue = 0
+ }
+ if (button.className == "operations") {
+  operation = button.textContent
+  
+
+ }
+ if (button.className == "number" && operation != undefined && storedValue != 0) {
+  storedValue2 = firstValue(numberStore2,button)
+  screen.textContent = storedValue2
+ }
+
+ if (button.className == "equal") {
+  result = operate(storedValue, storedValue2, operation)
+  screen.textContent = result
+  storedValue = result
+  clear(numberStore2)
+
+}
+
+
+
+ console.log(numberStore1)
+ console.log(numberStore2)
+ console.log(storedValue)
+})
+
+
+})
+
+
+
+
+
+
+/* ------Logic for old version 
 let storedValue = 0;
 let firstValue = 0;
 let secondValue = 0;
@@ -77,4 +144,4 @@ buttons.forEach((button) => {
       storedValue = 0;
     }
   });
-});
+}); */ 
